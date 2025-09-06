31041ac49af47fe973a33b0bd8f2878b305cb087
Sergey Antonov <clark.kent.2211@yandex.ru>
2025-09-06 17:36:52 +0300

    Add noise generation

--- src/game/levels/bar.ts
+++ src/game/levels/bar.ts
@@ -3,14 +3,14 @@ import { barFloor0, barFloor1, barFloor2, barFloor3, barWall0, barWall1, barWall
 import { images } from "../../resources/images";
 import { createCanvas, drawImage, getContext } from "../../utils/browser"
 import { generateRandomTileImage, generateTileImage } from "../../utils/image-generator";
-import { mathCeil, randomSelect } from "../../utils/math";
+import { mathCeil, mathRandom, randomSelect } from "../../utils/math";
 
 export const getBarStage = (): Stage => {
     const width = 400;
     const height = 150;
     const wallHeight = 100;
 
-    const floor = { image: generateRandomTileImage(width, height, [barFloor0, barFloor1, barFloor2, barFloor3]) }
+    const floor = { image: generateFloorImage(width, height) }
     const wall = { image: generateWallImage(width, wallHeight), x: 0, y: -wallHeight };
 
     return {
@@ -20,6 +20,16 @@ export const getBarStage = (): Stage => {
         wall,
         cameraPosition: { x: 0, y: 0 },
     }
+}
+
+const generateFloorImage = (width: number, height: number): number => {
+    let id = generateRandomTileImage(width, height, [barFloor0, barFloor1, barFloor2, barFloor3])
+
+    const image = images[id];
+
+    noise(10, image);
+
+    return id;
 }
 
 const generateWallImage = (width: number, height: number): number => {
@@ -49,5 +59,25 @@ const generateWallImage = (width: number, height: number): number => {
     drawImage(context, wallpaper1, columnStep * 2 - 17, 0);
     drawImage(context, wallpaper1, columnStep * 5 - 30, 0);
 
+    noise(10, image);
+
     return id;
+}
+
+export const noise = (offset: number, canvas: HTMLCanvasElement) => {
+    const context = getContext(canvas);
+    const offset2 = offset / 2;
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    let i = 0;
+    while (i < data.length) {
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
+        data[i] = r - offset2 + offset * mathRandom();
+        data[i + 1] = g - offset2 + offset * mathRandom();
+        data[i + 2] = b - offset2 + offset * mathRandom();
+        i += 4;
+    }
+    context.putImageData(imageData, 0, 0);
 }
